ff6f7016a8e110e9043f3369cf149d209021d077
VAGHASIYA JEEL <vaghasiyajeel2006@gmail.com>
2026-06-13 04:19:40 +0530

    Refactor MongoIO for MongoDB connection and methods

--- src/cloud_io/__init__.py
+++ src/cloud_io/__init__.py
@@ -1,38 +1,25 @@
+import os
 import pandas as pd
-import pymongo
-import os, sys
-import certifi
-from src.constants import *
-from src.exception import CustomException
+
 
 class MongoIO:
-    mongo_ins = None
-
     def __init__(self):
-        if MongoIO.mongo_ins is None:
-            mongo_db_url = os.getenv(MONGODB_URL_KEY)
-            if mongo_db_url is None:
-                raise Exception(f"Environment key: {MONGODB_URL_KEY} is not set.")
-            
-            client = pymongo.MongoClient(mongo_db_url, tlsCAFile=certifi.where())
-            
-            MongoIO.mongo_ins = client[MONGO_DATABASE_NAME]
-        
-        self.db = MongoIO.mongo_ins
+        try:
+            from pymongo import MongoClient
+            mongo_url = os.environ.get("MONGO_DB_URL", "")
+            if not mongo_url:
+                raise ValueError("MONGO_DB_URL not set")
+            self.client = MongoClient(mongo_url)
+            self.db = self.client["ajio_reviews"]
+        except Exception as e:
+            raise RuntimeError(f"MongoDB connection failed: {e}")
 
     def store_reviews(self, product_name: str, reviews: pd.DataFrame):
-        try:
-            collection_name = product_name.replace(" ", "_")
-            data_dict = reviews.to_dict(orient="records")
-            if data_dict:
-                self.db[collection_name].insert_many(data_dict)
-        except Exception as e:
-            raise CustomException(e, sys)
+        collection = self.db[product_name.replace(" ", "_")]
+        records = reviews.to_dict("records")
+        collection.insert_many(records)
 
-    def get_reviews(self, product_name: str):
-        try:
-            collection_name = product_name.replace(" ", "_")
-            data = list(self.db[collection_name].find())
-            return data
-        except Exception as e:
-            raise CustomException(e, sys)
+    def get_reviews(self, product_name: str) -> pd.DataFrame:
+        collection = self.db[product_name.replace(" ", "_")]
+        data = list(collection.find({}, {"_id": 0}))
+        return pd.DataFrame(data)
